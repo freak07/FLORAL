7a846f73f2e520a0f254bbbbbc4258faf64c8ec3
Andrey Konovalov <andreyknvl@google.com>
2019-07-23 19:58:52 +0200

    UPSTREAM: selftests, arm64: add a selftest for passing tagged pointers to kernel
    (Upstream commit 9ce1263033cd2ad393e2ff0df4a1c4ab4992c9df).
    
    This patch is a part of a series that extends kernel ABI to allow to pass
    tagged user pointers (with the top byte set to something else other than
    0x00) as syscall arguments.
    
    This patch adds a simple test, that calls the uname syscall with a
    tagged user pointer as an argument. Without the kernel accepting tagged
    user pointers the test fails with EFAULT.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Bug: 135692346
    Change-Id: I150694bf72706b5dd39939e38435c3aebf6ab2b3
    (cherry picked from commit 329f2c3b9d92b230d67b7072ac65a69ed06b8239)
    (cherry picked from commit f2feab0dddd30d8e5dace0c404edce0d13eb6fce)
    (cherry picked from commit d61496f71cf78a335f9c04c1a2f012b4f1ac78a5)

--- tools/testing/selftests/arm64/tags_test.c
+++ tools/testing/selftests/arm64/tags_test.c
@@ -1,8 +1,10 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+// SPDX-License-Identifier: GPL-2.0
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <sys/prctl.h>
 #include <sys/utsname.h>
 
 #define SHIFT_TAG(tag)		((uint64_t)(tag) << 56)
@@ -11,9 +13,17 @@
 
 int main(void)
 {
-	struct utsname utsname;
-	void *ptr = &utsname;
-	void *tagged_ptr = (void *)SET_TAG(ptr, 0x42);
-	int err = uname(tagged_ptr);
+	static int tbi_enabled = 0;
+	struct utsname *ptr, *tagged_ptr;
+	int err;
+
+	if (prctl(PR_SET_TAGGED_ADDR_CTRL, PR_TAGGED_ADDR_ENABLE, 0, 0, 0) == 0)
+		tbi_enabled = 1;
+	ptr = (struct utsname *)malloc(sizeof(*ptr));
+	if (tbi_enabled)
+		tagged_ptr = (struct utsname *)SET_TAG(ptr, 0x42);
+	err = uname(tagged_ptr);
+	free(ptr);
+
 	return err;
 }
