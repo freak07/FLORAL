6ae370204ec221aa96d864607a59b5c09d3232f8
freak07 <michalechner92@googlemail.com>
2019-10-29 08:51:32 +0100

    Revert "qcacld: disable bunch of more debug configs"
    This reverts commit 126c93e9835d7182dcd62ce58e31451ac971a864.
    
    (cherry picked from commit c7249b4fee5c980e50d0ab943888f286007c51d3)
    (cherry picked from commit 4457be7d57b8b1a49a78732dd4aa7fa71651f6f8)

--- drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_trace.c
+++ drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_trace.c
@@ -2848,7 +2848,6 @@ static inline void print_to_console(char *str_buffer)
 }
 #endif
 
-#if 0
 #ifdef MULTI_IF_NAME
 static const char *qdf_trace_wlan_modname(void)
 {
@@ -2932,7 +2931,6 @@ void qdf_trace_msg_cmn(unsigned int idx,
 	}
 }
 qdf_export_symbol(qdf_trace_msg_cmn);
-#endif
 
 QDF_STATUS qdf_print_setup(void)
 {
@@ -3355,7 +3353,6 @@ QDF_STATUS qdf_print_set_category_verbose(unsigned int idx,
 }
 qdf_export_symbol(qdf_print_set_category_verbose);
 
-#if 0
 bool qdf_print_is_category_enabled(unsigned int idx, QDF_MODULE_ID category)
 {
 	QDF_TRACE_LEVEL verbose_mask;
@@ -3427,7 +3424,6 @@ bool qdf_print_is_verbose_enabled(unsigned int idx, QDF_MODULE_ID category,
 	return verbose_enabled;
 }
 qdf_export_symbol(qdf_print_is_verbose_enabled);
-#endif
 
 #ifdef DBG_LVL_MAC_FILTERING
 
