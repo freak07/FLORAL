a01678278b3fd0fc7779f5c8ea7de51435eef908
freak07 <michalechner92@googlemail.com>
2019-11-21 06:37:10 +0100

    wakelock blocker: block diag_ws by default
    (cherry picked from commit f5c8e3b797bf664b673291495110e2e8cd7e64fd)

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				""
+#define LIST_WL_DEFAULT				"DIAG_WS"
 
 #define LENGTH_LIST_WL				255
 #define LENGTH_LIST_WL_DEFAULT		100
