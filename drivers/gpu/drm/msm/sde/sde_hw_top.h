946031d7104153f5f42b65638239b04cf1aee32e
freak07 <michalechner92@googlemail.com>
2019-11-25 14:53:36 +0100

    Revert "drm/msm/sde: Enable pp-split support for display driver"
    This reverts commit b8acf5e4013ed5f0f442866e853d0f624128fb87.

--- drivers/gpu/drm/msm/sde/sde_hw_top.h
+++ drivers/gpu/drm/msm/sde/sde_hw_top.h
@@ -46,7 +46,6 @@ struct traffic_shaper_cfg {
  * @split_flush_en: Allows both the paths to be flushed when master path is
  *              flushed
  * @split_link_en:  Check if split link is enabled
- * @pp_slave_intf: Check if this is pp slave interface
  */
 struct split_pipe_cfg {
 	bool en;
@@ -56,7 +55,6 @@ struct split_pipe_cfg {
 	u32 pp_split_index;
 	bool split_flush_en;
 	bool split_link_en;
-	bool pp_slave_intf;
 };
 
 /**
