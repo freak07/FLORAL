6563239d5f4a6ec083d0bff2a74cc9737019d5ad
freak07 <michalechner92@googlemail.com>
2019-12-12 08:45:39 +0100

    Revert "f2fs: convert wakelock APIs to wakeup_source"
    This reverts commit 3baf6921f67d38656503c1d38bda9f1813b0982d.

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -15,6 +15,7 @@
 #include <linux/freezer.h>
 #include <linux/fb.h>
 #include <linux/power_supply.h>
+#include <linux/wakelock.h>
 
 #include "f2fs.h"
 #include "node.h"
@@ -31,14 +32,14 @@ static inline void gc_set_wakelock(struct f2fs_sb_info *sbi,
 		struct f2fs_gc_kthread *gc_th, bool val)
 {
 	if (val) {
-		if (!gc_th->gc_wakelock.active) {
+		if (!wake_lock_active(&gc_th->gc_wakelock)) {
 			f2fs_info(sbi, "Catching wakelock for GC");
-			__pm_stay_awake(&gc_th->gc_wakelock);
+			wake_lock(&gc_th->gc_wakelock);
 		}
 	} else {
-		if (gc_th->gc_wakelock.active) {
+		if (wake_lock_active(&gc_th->gc_wakelock)) {
 			f2fs_info(sbi, "Unlocking wakelock for GC");
-			__pm_relax(&gc_th->gc_wakelock);
+			wake_unlock(&gc_th->gc_wakelock);
 		}
 	}
 }
@@ -189,7 +190,7 @@ int f2fs_start_gc_thread(struct f2fs_sb_info *sbi)
 
 	snprintf(buf, sizeof(buf), "f2fs_gc-%u:%u", MAJOR(dev), MINOR(dev));
 
-	wakeup_source_init(&gc_th->gc_wakelock, buf);
+	wake_lock_init(&gc_th->gc_wakelock, WAKE_LOCK_SUSPEND, buf);
 
 	sbi->gc_thread = gc_th;
 	init_waitqueue_head(&sbi->gc_thread->gc_wait_queue_head);
@@ -211,7 +212,7 @@ void f2fs_stop_gc_thread(struct f2fs_sb_info *sbi)
 	if (!gc_th)
 		return;
 	kthread_stop(gc_th->f2fs_gc_task);
-	wakeup_source_trash(&gc_th->gc_wakelock);
+	wake_lock_destroy(&gc_th->gc_wakelock);
 	kvfree(gc_th);
 	sbi->gc_mode = GC_NORMAL;
 	sbi->gc_thread = NULL;
