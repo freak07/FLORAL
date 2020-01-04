a85eb686373ba5569ccb51703dc8ce17640e3b1c
tbalden <illespal@gmail.com>
2020-01-04 07:32:41 -0800

    sde, kcal: remove excess log
    (cherry picked from commit b2f644f4b82637b6c341575faf7209af4ac01363)
    (cherry picked from commit 2c80769c8f18f8a02c6f1f32be0e109398483509)
    (cherry picked from commit 7d3407019c07d16be222ca793a5bd361db22d178)

--- drivers/gpu/drm/msm/sde/sde_color_processing.c
+++ drivers/gpu/drm/msm/sde/sde_color_processing.c
@@ -1065,7 +1065,7 @@ int sde_cp_crtc_set_property(struct drm_crtc *crtc,
 	}
 #if 1
 	if (prop_node->feature == SDE_CP_CRTC_DSPP_PCC) {
-		pr_info("%s pcc kad kcal\n",__func__);
+		//pr_info("%s pcc kad kcal\n",__func__);
 		g_pcc_crtc = crtc;
 		g_pcc_property = property;
 		g_pcc_val = val;
@@ -1134,7 +1134,7 @@ exit:
 #if 1
 void kcal_force_update(void) {
 	if (g_pcc_crtc) {
-		pr_info("%s force kad kcal\n",__func__);
+		//pr_info("%s force kad kcal\n",__func__);
 		sde_cp_crtc_set_property(g_pcc_crtc, g_pcc_property, g_pcc_val);
 	}
 }
