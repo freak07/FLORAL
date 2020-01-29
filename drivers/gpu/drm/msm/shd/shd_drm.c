13eaa7fc5c98b9f52420626885bd811555a7dac6
freak07 <michalechner92@googlemail.com>
2020-01-29 17:21:09 +0100

    Revert "Merge remote-tracking branch 'quic/dev/msm-4.14-display' into msm-4.14.c4"
    This reverts commit 175068bbbef503553a17cb3a75ad89144c7fb27b.

--- drivers/gpu/drm/msm/shd/shd_drm.c
+++ drivers/gpu/drm/msm/shd/shd_drm.c
@@ -242,7 +242,6 @@ static void shd_display_enable_base(struct drm_device *dev,
 	struct drm_connector *connector;
 	struct drm_crtc_state *crtc_state;
 	struct drm_connector_state *conn_state;
-	int ret;
 
 	SDE_DEBUG("enable base display %d\n", base->intf_idx);
 
@@ -275,12 +274,7 @@ static void shd_display_enable_base(struct drm_device *dev,
 	conn_state->best_encoder = base->encoder;
 	connector->encoder = base->encoder;
 
-	ret = drm_atomic_set_mode_for_crtc(crtc_state, &base->mode);
-	if (ret) {
-		SDE_ERROR("failed to set mode for crtc\n");
-		goto out;
-	}
-
+	drm_atomic_set_mode_for_crtc(crtc_state, &base->mode);
 	drm_mode_copy(&crtc_state->adjusted_mode, &base->mode);
 	drm_mode_copy(&base->crtc->mode, &base->mode);
 
@@ -330,8 +324,6 @@ static void shd_display_enable_base(struct drm_device *dev,
 
 	base->enabled = true;
 	base->enable_changed = true;
-out:
-	return;
 }
 
 static void shd_display_disable_base(struct drm_device *dev,
@@ -1049,13 +1041,13 @@ static int shd_drm_obj_init(struct shd_display *display)
 	memset(&info, 0x0, sizeof(info));
 	rc = shd_connector_get_info(NULL, &info, display);
 	if (rc) {
-		SDE_ERROR("shd get_info failed\n");
+		SDE_ERROR("shd get_info %d failed\n", i);
 		goto end;
 	}
 
 	encoder = sde_encoder_init_with_ops(dev, &info, &enc_ops);
 	if (IS_ERR_OR_NULL(encoder)) {
-		SDE_ERROR("shd encoder init failed\n");
+		SDE_ERROR("shd encoder init failed %d\n", i);
 		rc = -ENOENT;
 		goto end;
 	}
@@ -1080,7 +1072,7 @@ static int shd_drm_obj_init(struct shd_display *display)
 		priv->encoders[priv->num_encoders++] = encoder;
 		priv->connectors[priv->num_connectors++] = connector;
 	} else {
-		SDE_ERROR("shd connector init failed\n");
+		SDE_ERROR("shd %d connector init failed\n", i);
 		shd_drm_bridge_deinit(display);
 		sde_encoder_destroy(encoder);
 		rc = -ENOENT;
