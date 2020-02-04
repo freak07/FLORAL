72f811a22e181efef3e6b860e1e3455b737ed746
tbalden <illespal@gmail.com>
2020-02-04 11:02:08 -0800

    hbm: uci: dsi_backlight: fix HBM issues with AOD or screen-off stuck
    upon screen off, force hbm mode to OFF, to prevent possible
    collision with AOD/low power modes.
    
    (cherry picked from commit f86b03ae1d2828d47314c94acfcbe42a36551c22)

--- drivers/gpu/drm/msm/dsi-staging/dsi_backlight.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_backlight.c
@@ -69,26 +69,30 @@ static int uci_switch_hbm(int on) {
 static int uci_lux_level = -1;
 static bool uci_hbm_switch = false;
 static bool uci_hbm_use_ambient_light = false;
+static bool screen_wake_by_user = false;
+static bool screen_on = true;
 
 static void uci_sys_listener(void) {
-	int new_lux_level = uci_get_sys_property_int_mm("lux_level", 0, 0, 27000);
-	pr_info("%s hbm\n",__func__);
-	if (!uci_hbm_switch) {  // hbm switch is off..
-		if (last_hbm_mode) { // and it's set to hbm already in driver.. switch it off
-			uci_switch_hbm(0);
-		}
-	} else { // hbm switch is on...
-		if (new_lux_level==0 && uci_hbm_use_ambient_light) {
-			if (last_hbm_mode) {
+	if (screen_wake_by_user) {
+		int new_lux_level = uci_get_sys_property_int_mm("lux_level", 0, 0, 27000);
+		pr_info("%s hbm\n",__func__);
+		if (!uci_hbm_switch) {  // hbm switch is off..
+			if (last_hbm_mode) { // and it's set to hbm already in driver.. switch it off
 				uci_switch_hbm(0);
 			}
-		} else { // new lux level is high...let's switch it on
-			if (!last_hbm_mode || uci_lux_level == -1) { //... if it's not yet on...or fresh screen off/on cycle...
-				uci_switch_hbm(1);
+		} else { // hbm switch is on...
+			if (new_lux_level==0 && uci_hbm_use_ambient_light) {
+				if (last_hbm_mode) {
+					uci_switch_hbm(0);
+				}
+			} else { // new lux level is high...let's switch it on
+				if (!last_hbm_mode || uci_lux_level == -1) { //... if it's not yet on...or fresh screen off/on cycle...
+					uci_switch_hbm(1);
+				}
 			}
 		}
+		uci_lux_level = new_lux_level;
 	}
-	uci_lux_level = new_lux_level;
 }
 static void uci_user_listener(void) {
 	bool new_hbm_switch = !!uci_get_user_property_int_mm("hbm_switch", 0, 0, 1);
@@ -101,6 +105,19 @@ static void uci_user_listener(void) {
 		uci_sys_listener();
 	}
 }
+static void call_uci_sys(struct work_struct * call_uci_sys_work)
+{
+	uci_sys_listener();
+}
+static DECLARE_WORK(call_uci_sys_work, call_uci_sys);
+
+static void call_switch_hbm(struct work_struct * call_switch_hbm_work)
+{
+	last_hbm_mode = false;
+	uci_switch_hbm(0);
+}
+static DECLARE_WORK(call_switch_hbm_work, call_switch_hbm);
+
 static void ntf_listener(char* event, int num_param, char* str_param) {
         if (strcmp(event,NTF_EVENT_CHARGE_LEVEL) && strcmp(event, NTF_EVENT_INPUT)) {
                 pr_info("%s dsi_backlight ntf listener event %s %d %s\n",__func__,event,num_param,str_param);
@@ -108,10 +125,31 @@ static void ntf_listener(char* event, int num_param, char* str_param) {
 
         if (!strcmp(event,NTF_EVENT_SLEEP)) {
 		uci_lux_level = -1;
+		screen_wake_by_user = false;
+		screen_on = false;
+		schedule_work(&call_switch_hbm_work);
         }
-        if (!strcmp(event,NTF_EVENT_WAKE_BY_USER) || !strcmp(event,NTF_EVENT_WAKE_BY_FRAMEWORK)) {
+        if ((!strcmp(event,NTF_EVENT_LOCKED) && !!num_param)) { // locked
+		uci_lux_level = -1;
+		screen_wake_by_user = false;
+		schedule_work(&call_switch_hbm_work);
+	}
+        if (!strcmp(event,NTF_EVENT_WAKE_BY_USER)) {
 		// screen just on...set lux level -1, so HBM will be set again if needed...
 		uci_lux_level = -1;
+		screen_on = true;
+		screen_wake_by_user = true;
+	}
+	if (!strcmp(event,NTF_EVENT_WAKE_BY_FRAMEWORK)) {
+		uci_lux_level = -1;
+		screen_on = true;
+	}
+        if (!strcmp(event,NTF_EVENT_INPUT)) {
+		//event -> wake by user is sure...trigger sys listener
+		if (screen_on) {
+			screen_wake_by_user = true;
+			schedule_work(&call_uci_sys_work);
+		}
 	}
 }
 #endif
