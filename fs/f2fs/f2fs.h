b05544cec4a39c779be43442118cf7a0ecf20984
Jack Qiu <jack.qiu@huawei.com>
2020-07-24 16:55:28 +0800

    f2fs: space related cleanup
    Just for code style, no logic change
    1. delete useless space
    2. change spaces into tab
    
    Signed-off-by: Jack Qiu <jack.qiu@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit da478945e2a5da863a61df04a67610320aaafe56)

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1436,7 +1436,7 @@ struct f2fs_sb_info {
 	unsigned long last_time[MAX_TIME];	/* to store time in jiffies */
 	long interval_time[MAX_TIME];		/* to store thresholds */
 
-	struct inode_management im[MAX_INO_ENTRY];      /* manage inode cache */
+	struct inode_management im[MAX_INO_ENTRY];	/* manage inode cache */
 
 	spinlock_t fsync_node_lock;		/* for node entry lock */
 	struct list_head fsync_node_list;	/* node list head */
@@ -1516,7 +1516,7 @@ struct f2fs_sb_info {
 	unsigned int rapid_gc;			/* is rapid GC running */
 
 	/* for skip statistic */
-	unsigned int atomic_files;              /* # of opened atomic file */
+	unsigned int atomic_files;		/* # of opened atomic file */
 	unsigned long long skipped_atomic_files[2];	/* FG_GC and BG_GC */
 	unsigned long long skipped_gc_rwsem;		/* FG_GC only */
 
