38a66d7e1b436486aaacdbbcbfcca9e200c7b987
freak07 <michalechner92@googlemail.com>
2021-01-13 20:26:07 +0100

    Revert "usb: gadget: f_accessory: Make RX buffer size aligned to EP's MTU"
    This reverts commit a72dcd5ebd1fa7a8bf56616a4af415f5b68c9582.
    
    (cherry picked from commit ed31bd9de1e5276257edbbbc525032626c50b3b7)

--- drivers/usb/gadget/function/f_accessory.c
+++ drivers/usb/gadget/function/f_accessory.c
@@ -617,7 +617,8 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 {
 	struct acc_dev *dev = fp->private_data;
 	struct usb_request *req;
-	ssize_t r = count, xfer, len;
+	ssize_t r = count;
+	unsigned xfer;
 	int ret = 0;
 
 	pr_debug("acc_read(%zu)\n", count);
@@ -649,7 +650,7 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 requeue_req:
 	/* queue a request */
 	req = dev->rx_req[0];
-	req->length = len;
+	req->length = count;
 	dev->rx_done = 0;
 	ret = usb_ep_queue(dev->ep_out, req, GFP_KERNEL);
 	if (ret < 0) {
