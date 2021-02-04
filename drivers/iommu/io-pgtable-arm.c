bb60468823372bb3d32e8458b3a50991d0a4cc4b
freak07 <michalechner92@googlemail.com>
2021-02-04 10:51:42 +0100

    Revert "iommu: arm-smmu: fix check for need for preallocate memory"
    This reverts commit 0e692a8d233482bf5945d4057970a0fb1bb22942.

--- drivers/iommu/io-pgtable-arm.c
+++ drivers/iommu/io-pgtable-arm.c
@@ -634,8 +634,7 @@ static int arm_lpae_map_sg(struct io_pgtable_ops *ops, unsigned long iova,
 	arm_lpae_iopte prot;
 	struct scatterlist *s;
 	size_t mapped = 0;
-	int i;
-	int ret = -EINVAL;
+	int i, ret;
 	unsigned int min_pagesz;
 	struct io_pgtable_cfg *cfg = &data->iop.cfg;
 	struct map_state ms;
@@ -706,7 +705,7 @@ static int arm_lpae_map_sg(struct io_pgtable_ops *ops, unsigned long iova,
 out_err:
 	/* Return the size of the partial mapping so that they can be undone */
 	*size = mapped;
-	return ret;
+	return 0;
 }
 
 static void __arm_lpae_free_pgtable(struct arm_lpae_io_pgtable *data, int lvl,
