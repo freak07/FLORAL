7f8857c694e7d793adeafc6037062e1c5c9f8170
freak07 <michalechner92@googlemail.com>
2021-07-08 09:28:59 +0200

    Revert "Revert "mm: oom_kill: reap memory of a task that receives SIGKILL""
    This reverts commit 71dae9401951837ac1520d0c84f730679cf6778d.
    
    (cherry picked from commit 3c3765875171156c6b7e9bf4d4a811f867f29c8c)

--- kernel/signal.c
+++ kernel/signal.c
@@ -43,6 +43,8 @@
 #include <linux/compiler.h>
 #include <linux/posix-timers.h>
 #include <linux/cgroup.h>
+#include <linux/oom.h>
+#include <linux/capability.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/signal.h>
@@ -1344,8 +1346,11 @@ int group_send_sig_info(int sig, struct siginfo *info, struct task_struct *p)
 	ret = check_kill_permission(sig, info, p);
 	rcu_read_unlock();
 
-	if (!ret && sig)
+	if (!ret && sig) {
 		ret = do_send_sig_info(sig, info, p, true);
+		if (capable(CAP_KILL) && sig == SIGKILL)
+			add_to_oom_reaper(p);
+	}
 
 	return ret;
 }
