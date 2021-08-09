engstk/op9

hwtracing: Add entries from lahaina-coresight
cat lahaina-coresight.dtsi | grep primecell-periphid | cut -c29- | sed "s/>;//g;s/^/ETM4x_AMBA_ID(/g;s/$/),/g" | sort -u

eng.stk: add clocks for lahaina

Signed-off-by: engstk <eng.stk@sapo.pt>

drivers/hwtracing/google/coresight-clk-amba-placeholder.c
@@ -36,6 +36,14 @@ static const struct amba_id coresight_ids[] = {
 	ETM4x_AMBA_ID(0x000bb95a), /* Cortex-A72 */
 	ETM4x_AMBA_ID(0x000bb959), /* Cortex-A73 */
 	ETM4x_AMBA_ID(0x000bb9da), /* Cortex-A35 */
+	ETM4x_AMBA_ID(0x0003b968),
+	ETM4x_AMBA_ID(0x000bb908),
+	ETM4x_AMBA_ID(0x000bb961),
+	ETM4x_AMBA_ID(0x000bb962),
+	ETM4x_AMBA_ID(0x000bb966),
+	ETM4x_AMBA_ID(0x000bb968),
+	ETM4x_AMBA_ID(0x000bb969),
+	ETM4x_AMBA_ID(0x000bb999),
 	/* dynamic-replicator IDs */
 	{
 		.id = 0x000bb909,

freak07/FLORAL

adapted for zf8
(cherry picked from commit d7dc7a7893f3e5c7c1d5b558bdcc246f2dd41e00)

drivers/hwtracing/google/coresight-clk-amba-placeholder.c
@@ -36,6 +36,15 @@ static const struct amba_id coresight_ids[] = {
 	ETM4x_AMBA_ID(0x000bb95a), /* Cortex-A72 */
 	ETM4x_AMBA_ID(0x000bb959), /* Cortex-A73 */
 	ETM4x_AMBA_ID(0x000bb9da), /* Cortex-A35 */
+	ETM4x_AMBA_ID(0x0003b908),
+	ETM4x_AMBA_ID(0x0003b909),
+	ETM4x_AMBA_ID(0x0003b961),
+	ETM4x_AMBA_ID(0x0003b962),
+	ETM4x_AMBA_ID(0x0003b966),
+	ETM4x_AMBA_ID(0x0003b968),
+	ETM4x_AMBA_ID(0x0003b969),
+	ETM4x_AMBA_ID(0x0003b999),
+	ETM4x_AMBA_ID(0x000bb95d),
 	/* dynamic-replicator IDs */
 	{
 		.id = 0x000bb909,