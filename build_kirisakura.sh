408c08bbd45f6f1ffcdf5045350100094500a168
freak07 <michalechner92@googlemail.com>
2022-04-06 06:25:35 +0200

    buildscript: bump clang

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -13,7 +13,7 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r445002/bin
+export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r450784b/bin
 export PATH=${CLANG_PATH}:${PATH}
 export DTC_EXT=/home/miles/Downloads/DU_Tools/dtc-aosp
 export CLANG_TRIPLE=aarch64-linux-gnu-
